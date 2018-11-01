518305e6f6355d6e073457bc83fcb5de936eee2b
mcdomingo <mcdomingo@dons.usfca.edu>
2018-10-31 21:40:59 -0700

    added record count post

--- src/main/java/cs677/recordcount/RecordCountJob.java
+++ src/main/java/cs677/recordcount/RecordCountJob.java
@@ -3,20 +3,12 @@ package cs677.recordcount;
 import cs677.misc.FileCreator;
 import cs677.misc.JsonOutputFormat;
 import org.apache.hadoop.conf.Configuration;
-import org.apache.hadoop.fs.FileSystem;
-import org.apache.hadoop.fs.LocatedFileStatus;
 import org.apache.hadoop.fs.Path;
-import org.apache.hadoop.fs.RemoteIterator;
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
-import org.json.JSONObject;
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
 
 /** This is the main class. Hadoop will invoke the main method of this class. */
 public class RecordCountJob {
@@ -32,8 +24,8 @@ public class RecordCountJob {
       /* Mapper class */
       job.setMapperClass(RecordCountMapper.class);
 
-//      /* Combiner class */
-//      job.setCombinerClass(RecordCountReducer.class);
+      /* Combiner class */
+      //      job.setCombinerClass(RecordCountReducer.class);
 
       /* Reducer class */
       job.setReducerClass(RecordCountReducer.class);
@@ -57,36 +49,12 @@ public class RecordCountJob {
       /* Wait (block) for the job to complete... */
       boolean completed = job.waitForCompletion(true);
       if (completed) {
-        parseOutput(conf, outPath);
+        RecordCountPost.parseOutput(conf, outPath);
       }
       System.exit(completed ? 0 : 1);
 
     } catch (Exception e) {
       System.err.println(e.getMessage());
     }
-  }
-
-  private static void parseOutput(Configuration conf, Path path) throws IOException {
-    FileSystem fs = path.getFileSystem(conf);
-    RemoteIterator<LocatedFileStatus> fileStatusListIterator = fs.listFiles(path, false);
-    int total_count = 0;
-    int subreddit_count = 0;
-    while (fileStatusListIterator.hasNext()) {
-      LocatedFileStatus fileStatus = fileStatusListIterator.next();
-      if (fileStatus.isFile()) {
-        BufferedReader br =
-            new BufferedReader(new InputStreamReader(fs.open(fileStatus.getPath())));
-        String line = br.readLine();
-        while (line != null) {
-          JSONObject obj = new JSONObject(line);
-          int post_count = Integer.parseInt(obj.getString("value"));
-          total_count += post_count;
-          subreddit_count += 1;
-          line = br.readLine();
-        }
-      }
-    }
-    System.out.println(String.format("Number of subreddits: %d", subreddit_count));
-    System.out.println(String.format("Total number of entries: %d", total_count));
   }
 }
---
+++ src/main/java/cs677/recordcount/RecordCountPost.java
@@ -0,0 +1,48 @@
+package cs677.recordcount;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.LocatedFileStatus;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.fs.RemoteIterator;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class RecordCountPost {
+
+  public static void main(String[] args) {
+    try {
+      Configuration conf = new Configuration();
+      parseOutput(conf, new Path(args[0]));
+    } catch (IOException e) {
+      System.out.println(e.getMessage());
+    }
+  }
+
+  protected static void parseOutput(Configuration conf, Path path) throws IOException {
+    FileSystem fs = path.getFileSystem(conf);
+    RemoteIterator<LocatedFileStatus> fileStatusListIterator = fs.listFiles(path, false);
+    int total_count = 0;
+    int subreddit_count = 0;
+    while (fileStatusListIterator.hasNext()) {
+      LocatedFileStatus fileStatus = fileStatusListIterator.next();
+      if (fileStatus.isFile()) {
+        BufferedReader br =
+            new BufferedReader(new InputStreamReader(fs.open(fileStatus.getPath())));
+        String line = br.readLine();
+        while (line != null) {
+          //          JSONObject obj = new JSONObject(line);
+          //          int post_count = Integer.parseInt(obj.getString("value"));
+          int post_count = Integer.parseInt(line.split("\\s+", 2)[1]);
+          total_count += post_count;
+          subreddit_count += 1;
+          line = br.readLine();
+        }
+      }
+    }
+    System.out.println(String.format("Number of subreddits: %d", subreddit_count));
+    System.out.println(String.format("Total number of entries: %d", total_count));
+  }
+}
