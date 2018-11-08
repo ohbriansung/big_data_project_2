69d11084fc7d6ac73b69d36a44ccc9de597304fd
mcdomingo <mcdomingo@dons.usfca.edu>
2018-11-07 20:43:16 -0800

    put back toxic mapper stuff

--- src/main/java/cs677/Toxic/ToxicMapper.java
+++ src/main/java/cs677/Toxic/ToxicMapper.java
@@ -1,15 +1,21 @@
 package cs677.Toxic;
 
-import cs677.common.SentimentScorer;
 import org.apache.hadoop.io.DoubleWritable;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
 import org.json.JSONObject;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.StringTokenizer;
 
 public class ToxicMapper extends Mapper<LongWritable, Text, Text, DoubleWritable> {
+
+  private HashMap<String, Integer> sentimentMap = new HashMap<>();
 
   @Override
   protected void map(LongWritable key, Text value, Context context)
@@ -18,9 +24,31 @@ public class ToxicMapper extends Mapper<LongWritable, Text, Text, DoubleWritable
     String subreddit = obj.getString("subreddit");
     String body = obj.getString("body");
 
-    SentimentScorer scorer = new SentimentScorer();
-    double score = scorer.sentimentScore(body);
+    double sentScore = 0;
+    double count = 0;
+    StringTokenizer itr = new StringTokenizer(body);
+    // emit word, count pairs.
+    while (itr.hasMoreTokens()) {
+      count += 1;
+      sentScore += sentimentMap.getOrDefault(itr.nextToken(), 0);
+    }
 
-    context.write(new Text(subreddit), new DoubleWritable(score));
+    context.write(new Text(subreddit), new DoubleWritable(sentScore / count));
+  }
+
+  private void makeMap() throws IOException {
+    String line;
+    InputStream in = getClass().getResourceAsStream("/AFINN-111.txt");
+    BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+    while ((line = reader.readLine()) != null) {
+      String[] parts = line.split("\t");
+      if (parts.length >= 2) {
+        String key = parts[0];
+        int value = Integer.parseInt(parts[1]);
+        sentimentMap.put(key, value);
+      } else {
+        System.out.println("ignoring line: " + line);
+      }
+    }
   }
 }
--- src/main/java/cs677/life/LifeJob.java
+++ src/main/java/cs677/life/LifeJob.java
@@ -4,11 +4,14 @@ import cs677.Writables.SubreddtTimeWritable;
 import cs677.Writables.TextCountWritable;
 import cs677.common.Constants;
 import cs677.common.FileCreator;
+import cs677.common.SentimentScorer;
 import cs677.common.TimerStuff;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.DoubleWritable;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
+import org.apache.hadoop.io.Writable;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.hadoop.mapreduce.Reducer;
@@ -18,9 +21,14 @@ import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.DataInput;
+import java.io.DataOutput;
 import java.io.IOException;
 import java.time.Duration;
 import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+import java.time.format.DateTimeFormatter;
 
 public class LifeJob {
   private static final String AUTHOR_KEY = "user_key";
@@ -59,7 +67,7 @@ public class LifeJob {
       job.setMapperClass(LifeMapper.class);
       job.setMapOutputKeyClass(SubreddtTimeWritable.class);
       job.setMapOutputValueClass(TextCountWritable.class);
-      //
+
       //      /* Reducer */
       job.setReducerClass(LifeReducer.class);
       job.setOutputKeyClass(SubreddtTimeWritable.class);
@@ -130,47 +138,90 @@ public class LifeJob {
 
       String firstSentence = body.substring(0, cutIndex);
 
-      SentenceUpvotesWritable sentenceUpvotes =
-          new SentenceUpvotesWritable(firstSentence, (long) ups);
+//      LifeWritable sentenceUpvotes = new LifeWritable(firstSentence, (long) ups);
 
+      SentimentScorer scorer = new SentimentScorer();
+      double score = scorer.sentimentScore(body);
 
-
-      context.write(outKey, sentenceUpvotes);
+//      context.write(outKey, sentenceUpvotes);
     }
   }
 
   private static class LifeReducer
-      extends Reducer<
-          SubreddtTimeWritable,
-          SentenceUpvotesWritable,
-          SubreddtTimeWritable,
-          SentenceUpvotesWritable> {
+      extends Reducer<SubreddtTimeWritable, LifeWritable, SubreddtTimeWritable, LifeWritable> {
     public LifeReducer() {}
 
     @Override
-    protected void reduce(
-        SubreddtTimeWritable key, Iterable<SentenceUpvotesWritable> values, Context context)
+    protected void reduce(SubreddtTimeWritable key, Iterable<LifeWritable> values, Context context)
         throws InterruptedException, IOException {
 
-      for (SentenceUpvotesWritable value : values) {
+      for (LifeWritable value : values) {
         context.write(key, value);
       }
     }
   }
 
-  private static class SentenceUpvotesWritable extends TextCountWritable {
+  private static class LifeWritable implements Writable {
 
-    public SentenceUpvotesWritable() {
-      super();
+    private final Text body = new Text();
+    private final LongWritable upvotes = new LongWritable();
+    private final DoubleWritable score = new DoubleWritable();
+    private final Text subreddit = new Text();
+    private final LongWritable time = new LongWritable();
+    private LocalDateTime localDateTime;
+
+    public LifeWritable() {}
+
+    public LifeWritable(String body, long upvotes, double score, String subreddit) {
+      this.body.set(body);
+      this.upvotes.set(upvotes);
+      this.score.set(score);
+      this.subreddit.set(subreddit);
     }
 
-    public SentenceUpvotesWritable(String text, long upvotes) {
-      super(text, upvotes);
+    public void setTime(long time) {
+      this.time.set(time);
+      localDateTime = LocalDateTime.ofEpochSecond(time, 0, ZoneOffset.UTC);
+    }
+
+    @Override
+    public void write(DataOutput output) throws IOException{
+      body.write(output);
+      upvotes.write(output);
+      score.write(output);
+      subreddit.write(output);
+      time.write(output);
+    }
+
+    @Override
+    public void readFields(DataInput input) throws IOException {
+
     }
 
     @Override
     public String toString() {
-      return "{\"ups\": " + getCount() + ", \"body\": \"" + getCount() + "\"}";
+      StringBuilder sb = new StringBuilder();
+
+      sb.append("{");
+      if (localDateTime != null) {
+        sb.append("\"time\": \"");
+        sb.append(localDateTime.format(DateTimeFormatter.ISO_INSTANT));
+        sb.append("\", ");
+      }
+      sb.append("\"subreddit\": ");
+      sb.append(subreddit);
+      sb.append(", \"ups\": ");
+      sb.append(upvotes);
+      sb.append(", \"sentimentScore\": ");
+      sb.append(score);
+      sb.append(", \"body\": \"");
+      sb.append(body);
+      if (localDateTime != null) {
+        sb.append("\"timeLong\": \"");
+        sb.append(time);
+      }
+      sb.append("}");
+      return sb.toString();
     }
   }
 }
