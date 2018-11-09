e578ea64b3f4af3e2c1e036289299e708487b7cf
scott <tyung@dons.usfca.edu>
2018-11-09 16:42:13 -0500

    Subreddit writable and array wriable

--- src/main/java/cs677/BackStory/BackStoryReducer.java
+++ src/main/java/cs677/BackStory/BackStoryReducer.java
@@ -4,6 +4,7 @@ import cs677.Writables.BackGroundKey;
 import cs677.Writables.BackGroundWritable;
 
 import cs677.Writables.Subreddits;
+import cs677.Writables.TextCountWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Reducer;
 
@@ -11,43 +12,29 @@ import java.io.IOException;
 import java.util.*;
 
 
-public class BackStoryReducer extends Reducer<BackGroundKey, BackGroundWritable, BackGroundKey,Text> {
+public class BackStoryReducer extends Reducer<Text, BackGroundWritable, Text,BackGroundWritable> {
 
     @Override
-    protected void reduce(BackGroundKey key, Iterable<BackGroundWritable> values, Context context) throws IOException, InterruptedException {
+    protected void reduce(Text key, Iterable<BackGroundWritable> values, Context context) throws IOException, InterruptedException {
         HashMap<String,Long> likedsubreddits = new HashMap<>();
         String subreddit;
-        BackGroundWritable backGroundWritable = new BackGroundWritable(0,0,"none","none",0);
+        ArrayList<Subreddits> top10  = new ArrayList<>();
+        BackGroundWritable backGroundWritable = new BackGroundWritable(0,0,"none",0,"none",top10);
         for(BackGroundWritable value : values){
             subreddit = value.getLikes().toString();
             long count = 1;
             count += likedsubreddits.getOrDefault(subreddit, 0L);
             likedsubreddits.put(subreddit,count);
 
-            key.append(value.getReadability_score().get(),1);
             backGroundWritable.append(value.getUpvotes().get(),value.getReadability_score().get(),value.getLocation().toString(),value.getCommentcount().get());
         }
 
-        ArrayList<Subreddits> top10 = tolist(likedsubreddits);
-        context.write(key, new Text(buildtext(backGroundWritable,top10)));
+        top10 = tolist(likedsubreddits);
+        backGroundWritable.updateList(top10);
+        context.write(key, backGroundWritable);
     }
 
 
-    private String buildtext(BackGroundWritable backGroundWritable, ArrayList<Subreddits> top10){
-        StringBuilder stringBuilder = new StringBuilder();
-        stringBuilder.append("\n");
-        stringBuilder.append(backGroundWritable.toString());
-        if(top10.size() > 0) {
-            stringBuilder.append("\nLikedSubreddits: ");
-            for (int i = 0; i < top10.size(); i++) {
-                stringBuilder.append("\n\t" + top10.get(i).toString());
-            }
-        }else{
-            stringBuilder.append("Something is very wrong");
-        }
-        stringBuilder.append("\n\n");
-        return stringBuilder.toString();
-    }
 
 
 
---
+++ src/main/java/cs677/Writables/Subredditlist.java
@@ -0,0 +1,53 @@
+package cs677.Writables;
+
+import org.apache.hadoop.io.ArrayWritable;
+import org.apache.hadoop.io.Writable;
+
+import java.io.DataInput;
+import java.io.DataOutput;
+import java.io.IOException;
+
+public class Subredditlist extends ArrayWritable {
+
+    public Subredditlist() {
+        super(Subreddits.class);
+    }
+
+    public Subredditlist(Writable[] values) {
+        super(Subreddits.class, values);
+    }
+
+
+    @Override
+    public void write(DataOutput out) throws IOException {
+        super.write(out);
+    }
+
+    @Override
+    public void readFields(DataInput in) throws IOException {
+        super.readFields(in);
+    }
+
+    @Override
+    public Writable[] get() {
+        return (super.get());
+    }
+
+    @Override
+    public String toString() {
+        Writable[] writables = super.get();
+        Subreddits[] subreddits = new Subreddits[writables.length];
+        for(int i = 0; i < writables.length; i++){
+            subreddits[i] = (Subreddits) writables[i];
+        }
+        StringBuilder sb = new StringBuilder();
+        sb.append("{");
+        for (Subreddits writable : subreddits) {
+            sb.append("\"");
+            sb.append(writable.getSubreddit());
+            sb.append(", ");
+        }
+        sb.append("}");
+        return sb.toString();
+    }
+}
