4e13189bbd1e38acc51431704c8a624015a464a0
mcdomingo <mcdomingo@dons.usfca.edu>
2018-11-11 12:04:45 -0800

    intial analysis done

--- src/main/java/cs677/life/LifeJob.java
+++ src/main/java/cs677/life/LifeJob.java
@@ -1,7 +1,6 @@
 package cs677.life;
 
 import cs677.common.*;
-import org.apache.commons.lang3.StringEscapeUtils;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.conf.Configured;
 import org.apache.hadoop.fs.Path;
